89962fd5ff952759bc229e5545f957dd4a3b4407
Korey Arsenault <koreyarsenault@gmail.com>
2023-07-16 21:38:38 -0400

    initial commit of plugin

---
+++ gui/textlogger.py
@@ -0,0 +1,14 @@
+import logging
+
+
+class QPlainTextEditLogger(logging.Handler):
+    def __init__(self, widget):
+        super().__init__()
+        self.widget = widget
+        self.widget.setReadOnly(True)
+                    
+
+    def emit(self, record):
+        msg = self.format(record)
+        self.widget.appendPlainText(msg)
+    
